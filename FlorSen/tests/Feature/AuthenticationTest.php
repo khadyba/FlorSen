3b56fc29d112962777a51c6ab19d8cacdab2e7b6
khady <bak92147@gmail.comgit config --global user.name khady>
2024-01-20 12:21:51 +0000

    j'ai terminer les test unitaire concernant le logout,lister les clients et jardinier inscrit par l'administrateur.

--- FlorSen/tests/Feature/AuthenticationTest.php
+++ FlorSen/tests/Feature/AuthenticationTest.php
@@ -4,8 +4,7 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 class AuthenticationTest extends TestCase
 {
 
@@ -28,6 +27,53 @@ class AuthenticationTest extends TestCase
             'email' => 'une adresse  email doit etre fournie',
             'password' => 'Le mot de passe est requis et doit avoir au minimum 7 caractères',
         ]);
-    }   
+    }
+    public function    test_user_can_logout()
+    {
+        $user = User::factory()->create([
+            'password' => bcrypt('password'),
+        ]);
+        $token = JWTAuth::fromUser($user);
+        $response = $this->withHeader('Authorization', 'Bearer' . $token)
+                         ->post('/api/logout');
+        $response->assertStatus(200);
+    }
+     public function test_user_can_viewAni()
+     {
+        $user = User::factory()->create([
+            'email'=>'admin@example.com',
+            'password'=>bcrypt('password')
+        ]);
+        $token = JWTAuth::fromUser($user);
+        
+        $response = $this->withHeader('Authorization', 'Bearer' . $token)
+                         ->get('api/listJardinier');
+        $response->assertStatus(200);
+     }
+   
+       public function test_user_can_vienAny()
+       {
+        $user = User::factory()->create([
+            'email'=>'admin@example.fr',
+            'password'=>bcrypt('password')
+        ]);
+        $token = JWTAuth::fromUser($user);
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
+                         ->get('api/listClients');
+        $response->assertStatus(200);
+       }
+     
+
+       public function test_user_can_update()
+       {
+        $user = User::factory()->create([
+            'email'=>'khady@example.fr',
+            'password'=>bcrypt('password')
+        ]);
+        $token = JWTAuth::fromUser($user);
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
+                         ->post('api/modifierProfil/{id}');
+        $response->assertStatus(403);
+       }
 }
 
